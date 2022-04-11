a5bcd1d89e39864e79ab98457b07447cb234b145
Natalja Smirnova <natalja_smirnova@swissre.com>
2022-04-11 13:00:53 +0300

    Added my project

---
+++ cypress/integration/fizzBuzzTest.spec.js
@@ -0,0 +1,52 @@
+import fizzBuzz from '../integration/fizzBuzzTest.spec'
+// math exports a default object with methods
+import math from 'mathjs'
+
+describe('Unit Test Application Code', function () {
+    // const { add, divide, multiply, subtract } = math;
+
+    before(() => {
+        // check if the import worked correctly
+        // expect(add, 'add').to.be.a('function')
+    });
+
+    // context('math.js', function () {
+    //     it('can add numbers', function () {
+    //         expect(add(1, 2)).to.eq(3)
+    //     });
+    //
+    //     it('can subtract numbers', function () {
+    //         expect(subtract(5, 12)).to.eq(-7)
+    //     });
+    //
+    //     it('can divide numbers', function () {
+    //         expect(divide(27, 9)).to.eq(3)
+    //     });
+    //
+    //     it('can multiple numbers', function () {
+    //         expect(multiply(5, 4)).to.eq(20)
+    //     })
+    // });
+
+    context('fizzBuzz.js', function () {
+        function numsExpectedToEq (arr, expected) {
+            // loop through the array of nums and make
+            // sure they equal what is expected
+            arr.forEach((i) => {
+                expect(fizzBuzz(i)).to.eq(expected)
+            })
+        }
+
+        it('returns "fizz" when number is multiple of 3', function () {
+            numsExpectedToEq([9, 12, 18], 'fizz')
+        });
+
+        it('returns "buzz" when number is multiple of 5', function () {
+            numsExpectedToEq([10, 20, 25], 'buzz')
+        });
+
+        it('returns "fizzBuzz" when number is multiple of both 3 and 5', function () {
+            numsExpectedToEq([15, 30, 60], 'fizzBuzz')
+        })
+    })
+});
